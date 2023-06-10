08be40d4f6275259b6b9ce9cc0fcedd213523580
ChristopherVuvu <vuvumumo93@gmail.com>
2023-06-11 00:26:08 +0300

    addded

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,20 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    create_table :students do |t|
+      t.string :name
+  end
+  end
+end
+
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[6.1]
+  def change
+      add_column :students, :grade, :integer
+      add_column :students, :birthdate, :string
+  end
+end
+
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[6.1]
+  def change
+      change_column :students, :birthdate, :datetime
   end
 end
